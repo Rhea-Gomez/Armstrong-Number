cd1eeb702ab0f8a978451c830def2189b0214466
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-11 01:43:51 +0530

    Assignment on Armstrong Number

---
+++ ArmstrongNumber/ArmstrongNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ArmstrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, remainder, sum = 0, temp;
+            Console.Write("Enter a number to check whether it is Armstrong number or not: ");
+            num = int.Parse(Console.ReadLine());
+
+            temp = num;
+            while (temp > 0)
+            {
+                remainder = temp % 10;
+                sum = sum + (remainder * remainder * remainder);
+                temp = temp / 10;
+            }
+            if (sum == num)
+                Console.Write(num + " is an Armstrong Number.");
+            else
+                Console.Write(num + " is not an Armstrong Number.");
+        }
+    }
+    }
+
